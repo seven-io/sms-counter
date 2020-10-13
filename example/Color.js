ce0970ea156c3d0f3c1667e743df177510214d39
André Matthies <matthiez@gmail.com>
2020-10-13 15:10:21 +0200

    example: consider partial encodings

--- example/Color.js
+++ example/Color.js
@@ -1,25 +1,33 @@
-const Color = {
-    escape: 'sandybrown',
-    notGsm: 'darkgreen',
-    unicode: 'lightgreen',
-    regular: 'lightgray',
-    udh: 'lightblue',
+class Color {
+    scheme = {
+        escape: 'sandybrown',
+        notGsm: 'darkgreen',
+        unicode: 'lightgreen',
+        regular: 'lightgray',
+        udh: 'lightblue',
+    };
+
+    constructor(detail) {
+        this.detail = detail;
+    }
 
     get(char) {
         if (char.escape || '\n' === char.character) {
-            return Color.escape;
+            return this.scheme.escape;
         }
 
         if (char.udh) {
-            return Color.udh;
+            return this.scheme.udh;
         }
 
         if (!char.gsm7) {
-            return Color.notGsm; // force unicode as char is not in GSM charset
+            return this.scheme.notGsm; // force unicode as char is not in GSM charset
         }
 
         return char.unicode
-            ? Color.unicode // unicode encoded char in GSM charset
-            : Color.regular; // regular 7-bit character present in GSM charset;
-    },
-};
+            ? this.scheme.unicode // unicode encoded char in GSM charset
+            : 'UCS2' === this.detail.encoding
+                ? this.scheme.unicode
+                : this.scheme.regular; // regular 7-bit character present in GSM charset;
+    }
+}
--- example/script.js
+++ example/script.js
@@ -6,22 +6,22 @@ const displayedChar = char => ' ' === char
     ? '␣' : '\n' === char
         ? 'LF' : char;
 
-const msgChars = msg => msg.map(char => `
-                    <li 
+const msgChars = (color, msg) => msg.map(c => `<li 
                     class='sms77-letter' 
-                    style='background-color: ${Color.get(char)};' 
-                    title='${Object.keys(char).map(k => `${k}: ${char[k]}`).join('\n')}'
+                    style='background-color: ${color.get(c)};' 
+                    title='${Object.keys(c).map(k => `${k}: ${c[k]}`).join('\n')}'
                     >
-                        ${displayedChar(char.character)}
+                        ${displayedChar(c.character)}
                     </li>
                 `).join('');
 
 document.addEventListener('sms77io_counter_input',
-    ev => Object.keys(ev.detail).forEach(key => {
-        const value = ev.detail[key];
+    ({detail}) => Object.keys(detail).forEach(k => {
+        const value = detail[k];
 
-        document.getElementById(key).innerHTML =
-            'messages' === key
-                ? value.map(msg => `<li><ul>${msgChars(msg)}</ul></li>`).join('')
+        document.getElementById(k).innerHTML =
+            'messages' === k
+                ? value.map(msg =>
+                    `<li><ul>${msgChars(new Color(detail), msg)}</ul></li>`).join('')
                 : value;
     }));
