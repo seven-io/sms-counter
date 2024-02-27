8ed21e1f0f9c48fbbb6b5afc7ca2112379d5cd11
André Matthies <matthiez@gmail.com>
2024-02-27 09:53:35 +0100

    fix escape character for subsequent special chars

--- src/lib/getMessages.ts
+++ src/lib/getMessages.ts
@@ -4,6 +4,8 @@ import {getCharEncoding} from './getCharEncoding'
 import {isGSM7} from './isGSM7'
 import {GSM_7BIT_ABC, GSM_7BIT_ABC_EXTENSION} from './chars'
 
+const ESCAPE_CHRACTER = 'ESC'
+
 const getHeaders = (count: number, index: number) => [
     '05',
     '00',
@@ -35,38 +37,36 @@ export const getMessages = (
         for (const char of msgChars) {
             const charIndex = (headerCount - 1) + i
 
+            if (!unicode && !GSM_7BIT_ABC.includes(char)) unicode = true
+
             if (!unicode) {
-                if (!GSM_7BIT_ABC.includes(char)) {
-                    unicode = true
-                } else {
-                    if (GSM_7BIT_ABC_EXTENSION.includes(char)) {
-                        msg[charIndex - 1] = 'ESC'
-                    }
+                if (GSM_7BIT_ABC_EXTENSION.includes(char)) {
+                    console.log('extension character in a non-unicode message, ADDING ESCAPE CHARACTER', {charIndex, char, msg})
+
+                    msg.push(ESCAPE_CHRACTER)
                 }
             }
 
-            msg[charIndex] = char
+            msg.push(char)
 
             i++
         }
 
         startIndex = endIndex
 
-        if (isMulti) {
-            msg.splice(0, headerCount, ...headers)
-        }
+        if (isMulti) msg.splice(0, headerCount, ...headers)
 
         messages.push([
             ...msg
                 .filter((m: any) => m)
                 .map((character: any): Char => {
-                    const isGSM7Char = character.length > 1 && 'ESC' !== character
+                    const isGSM7Char = character.length > 1 && ESCAPE_CHRACTER !== character
 
                     return {
                         character,
                         encoding: isGSM7Char
                             ? 'GSM7' : getCharEncoding(getCharLength(encoding, character)),
-                        escape: isGSM7Char ? false : 'ESC' === character,
+                        escape: isGSM7Char ? false : ESCAPE_CHRACTER === character,
                         gsm7: isGSM7Char ? true : isGSM7(character),
                         length: 1,
                         udh: isGSM7Char,
